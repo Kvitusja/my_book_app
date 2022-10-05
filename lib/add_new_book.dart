6aae477b27e6646b7ccb77dd974885760a003a54
natal <kvitusja@gmail.com>
2022-10-05 21:40:53 +0200

    next

--- lib/add_new_book.dart
+++ lib/add_new_book.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:dotted_border/dotted_border.dart';
 
 class AddNewBook extends StatefulWidget {
-  const AddNewBook({Key? key,}) : super(key: key);
+  const AddNewBook({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<AddNewBook> createState() => _AddNewBookState();
@@ -12,12 +15,41 @@ class _AddNewBookState extends State<AddNewBook> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      elevation: 0,
-      title: const Text(
-        "My book app",
-        style: TextStyle(color: Colors.black),
+        elevation: 0,
+        title: const Text(
+          "My book app",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
       ),
-      backgroundColor: Colors.white,
-    ),);
+      body: Column(
+        children: [
+          DottedBorder(
+            strokeWidth: 3,
+            dashPattern: const [3, 2],
+            borderType: BorderType.RRect,
+            radius: const Radius.circular(16.0),
+            child: const TextField(),
+          ),
+          Container(
+              height: 65,
+              width: 310,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2.5),
+                  color: Colors.orangeAccent,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20.0),)
+              ),
+              child: const TextField()),
+      DottedBorder(
+        strokeWidth: 3,
+        dashPattern: const [3, 2],
+        borderType: BorderType.RRect,
+        radius: const Radius.circular(16.0),
+        child: const TextField(),
+      ),
+        ],
+      ),
+    );
   }
 }
